c485b6e0341bf93aabb5bf82551027f678aa1585
Đặng Quý <160294940+QuyDang1108@users.noreply.github.com>
2024-12-24 22:52:31 +0700

    docker (#18)
    * docker
    
    * setup
    
    * setup
    
    * setup
    
    * setup
    
    * setup
    
    * setup
    
    * setup

--- backend/src/main/java/com/furnistyle/furniturebackend/controllers/AuthController.java
+++ backend/src/main/java/com/furnistyle/furniturebackend/controllers/AuthController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,5 +46,10 @@ public class AuthController {
         HttpServletResponse response
     ) throws IOException {
         service.refreshToken(request, response);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Deploy successfully!");
     }
 }
